4f78dfc1cf24626fce651bec4a713d5e6be19df6
yuto.ohta <yuto-ota@dac.co.jp>
2023-06-02 00:44:23 +0000

    add contollers

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="prose ml-4">
+        <h2>メッセージ 一覧</h2>
+    </div>
+
+    @if (isset($tasks))
+        <table class="table table-zebra w-full my-4">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>メッセージ</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td><a class="link link-hover text-info" href="#">{{ $task->id }}</a></td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+    
+    {{-- メッセージ作成ページへのリンク --}}
+    <a class="btn btn-primary" href="{{ route('tasks.create') }}">新規メッセージの投稿</a>
+
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="prose ml-4">
+        <h2>id = {{ $task->id }} のメッセージ詳細ページ</h2>
+    </div>
+
+    <table class="table w-full my-4">
+        <tr>
+            <th>id</th>
+            <td>{{ $task->id }}</td>
+        </tr>
+
+        <tr>
+            <th>メッセージ</th>
+            <td>{{ $task->content }}</td>
+        </tr>
+    </table>
+    
+    {{-- メッセージ編集ページへのリンク --}}
+    <a class="btn btn-outline" href="{{ route('tasks.edit', $task->id) }}">このメッセージを編集</a>
+    
+    {{-- メッセージ削除フォーム --}}
+    <form method="POST" action="{{ route('tasks.destroy', $task->id) }}" class="my-2">
+        @csrf
+        @method('DELETE')
+        
+        <button type="submit" class="btn btn-error btn-outline" 
+            onclick="return confirm('id = {{ $task->id }} のメッセージを削除します。よろしいですか？')">削除</button>
+    </form>
+    
+@endsection
